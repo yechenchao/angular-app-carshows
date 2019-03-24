d7de4fdf5dbc0322ac85931e96301d0d6c1c93b8
Peter Ye <yechenchao@gmail.com>
2019-03-24 15:58:16 +1100

    Structured output data

--- src/app/shared/services/car-shows.service.ts
+++ src/app/shared/services/car-shows.service.ts
@@ -49,18 +49,28 @@ export class CarShowsService {
 
   processCarShowsData() {
     this.rawCarShows = _.chain(this.rawCarShows)
-      .uniq(rawCarShow => {
-        return JSON.stringify(rawCarShow);
-      })
-      .filter(rawCarShow => {
-        return rawCarShow['make'] && rawCarShow['name'] && rawCarShow['model'];
-      })
+      .uniq(rawCarShow => JSON.stringify(rawCarShow))
+      .filter(rawCarShow => _.keys(rawCarShow).length === 3 && rawCarShow['make'] && rawCarShow['name'] && rawCarShow['model'])
       .sortBy('make')
       .groupBy('make')
       .map((value, key) => {
-        let cars = _.map(value, car => {
-          return _.omit(car, 'make')
-        });
+        const cars = _.chain(value)
+          .map(car => _.omit(car, 'make'))
+          .groupBy('model')
+          .map((val, ke) => {
+            console.log(val);
+
+            const ca = _.chain(val)
+              .map(v => _.omit(v, 'model'))
+              .pluck('name')
+              .value();
+
+            return {
+              model: ke,
+              shows: ca,
+            };
+          })
+          .value();
 
         return {
           make: key,
